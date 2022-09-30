46180a3af9dad15e079dd5d1f7eb1406b2b4605c
Edwin Rivera <edriver11@elpoli.edu.co>
2022-09-30 13:16:56 -0500

    conexion con sqlite probada usando extension en vscode y guarando un carro con el metodo post

--- carsharing.py
+++ carsharing.py
@@ -1,14 +1,29 @@
 import uvicorn
 
+from sqlmodel import create_engine, SQLModel, Session
+
 from fastapi import FastAPI, HTTPException
 
 import db_array
-from schemas import CarInput, CarOutput, TripInput, TripOutput, load_db, save_db
+from schemas import Car, CarInput, CarOutput, TripInput, TripOutput, load_db, save_db
+
+app = FastAPI(title="Car Sharing") # app = Rest service
 
 db_array = db_array.db
 db = load_db()
 
-app = FastAPI(title="Car Sharing") # app = Rest service
+engine = create_engine(
+  "sqlite:///carsharing.db", # string de coneccion
+  connect_args={"check_same_thread": False}, # Needed for SQLite
+  echo=True # Log generated SQL y los muestra por consola
+)
+
+# inicia antes de cualquier request del cliente
+@app.on_event("startup")
+def on_startup():
+  SQLModel.metadata.create_all(engine) # cheque si la db existe, sino la crea
+
+
 
 # @app.get("/api/cars")
 # def get_cars():
@@ -49,13 +64,26 @@ def car_by_id(id: int) -> dict:
     raise HTTPException(status_code=404, detail=f"No car with id {id} in the db")
 
 
-@app.post("/api/cars", response_model=CarOutput)
-def add_car(car: CarInput) -> CarOutput:
-  new_car = CarOutput(size=car.size, doors=car.doors, fuel=car.fuel, 
-                      transmission=car.transmission, id=len(db)+1)
-  db.append(new_car)
-  save_db(db)
-  return new_car
+# funciona con db en json file
+# @app.post("/api/cars", response_model=CarOutput)
+# def add_car(car: CarInput) -> CarOutput:
+#   new_car = CarOutput(size=car.size, doors=car.doors, fuel=car.fuel, 
+#                       transmission=car.transmission, id=len(db)+1)
+#   db.append(new_car)
+#   save_db(db)
+#   return new_car
+
+# funciona con sqlite
+@app.post("/api/cars", response_model=Car)
+def add_car(car_input: CarInput) -> Car:
+  # trasaccion con la db, o todo esta bueno y se guarda o algo falla y no se guarda en la db
+  with Session(engine) as session:
+    new_car = Car.from_orm(car_input) # permite trabajara correctamente con relaciones entre objetos
+    session.add(new_car)
+    session.commit()
+    session.refresh(new_car) # en esta parte se crea el id cuando se salva en la db
+    return new_car
+  
 
 @app.delete("/api/cars/{id}", status_code=204)
 def remove_car(id: int) -> None:
