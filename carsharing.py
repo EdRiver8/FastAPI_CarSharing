3a56b2bbd6326b00136bd39bd6037d9094bedc0e
Edwin Rivera <edriver11@elpoli.edu.co>
2022-09-29 20:43:35 -0500

    Creando el delete y el put

--- carsharing.py
+++ carsharing.py
@@ -8,7 +8,7 @@ from schemas import CarInput, CarOutput, load_db, save_db
 db_array = db_array.db
 db = load_db()
 
-app = FastAPI() # app = Rest service
+app = FastAPI(title="Car Sharing") # app = Rest service
 
 # @app.get("/api/cars")
 # def get_cars():
@@ -49,13 +49,38 @@ def car_by_id(id: int) -> dict:
     raise HTTPException(status_code=404, detail=f"No car with id {id} in the db")
 
 
-@app.post("/api/cars")
+@app.post("/api/cars", response_model=CarOutput)
 def add_car(car: CarInput) -> CarOutput:
   new_car = CarOutput(size=car.size, doors=car.doors, fuel=car.fuel, 
                       transmission=car.transmission, id=len(db)+1)
   db.append(new_car)
   save_db(db)
   return new_car
+
+@app.delete("/api/cars/{id}", status_code=204)
+def remove_car(id: int) -> None:
+  matches = [car for car in db if car.id == id]
+  if matches:
+    car = matches[0];
+    db.remove(car);
+    save_db(db)
+  else:
+    raise HTTPException(status_code=404, detail=f"No car with id = {id}")
+  
+
+@app.put("/api/cars/{id}")
+def change_car(id: int, new_data: CarInput) -> CarOutput:
+  matches = [car for car in db if car.id == id]
+  if matches:
+    car = matches[0]
+    car.fuel = new_data.fuel
+    car.transmission = new_data.transmission
+    car.size = new_data.size
+    car.doors = new_data.doors
+    save_db(db)
+    return car
+  else:
+    raise HTTPException(status_code=404, detail=f"No car with id = {id}")
 
 
 
