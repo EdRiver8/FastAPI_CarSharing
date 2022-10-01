318c5415978a8c0ba7738a54bd3877d2a9fc2104
Edwin Rivera <edriver11@elpoli.edu.co>
2022-10-01 12:00:44 -0500

    inyeccion dependencias con session en los path/routes/endpoints; relacionamiento de tablas entre Car y Trip

--- carsharing.py
+++ carsharing.py
@@ -1,16 +1,16 @@
 import uvicorn
 
-from sqlmodel import create_engine, SQLModel, Session
+from sqlmodel import create_engine, SQLModel, Session, select
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 
 import db_array
-from schemas import Car, CarInput, CarOutput, TripInput, TripOutput, load_db, save_db
+from schemas import Car, CarInput, CarOutput, TripInput, TripOutput, Trip,load_db, save_db
 
 app = FastAPI(title="Car Sharing") # app = Rest service
 
-db_array = db_array.db
-db = load_db()
+# db_array = db_array.db
+# db = load_db()
 
 engine = create_engine(
   "sqlite:///carsharing.db", # string de coneccion
@@ -23,6 +23,10 @@ engine = create_engine(
 def on_startup():
   SQLModel.metadata.create_all(engine) # cheque si la db existe, sino la crea
 
+# sirve como inyeccion de dependencias
+def get_session():
+  with Session(engine) as session:
+    yield session # funcion generadora, da proteccion de rollback en caso que algo salga mal
 
 
 # @app.get("/api/cars")
@@ -43,23 +47,36 @@ def on_startup():
 #     return result
   
   
-# usando la db en formato json
+# # usando la db en formato json
+# @app.get("/api/cars")
+# def get_cars(size: str|None = None, doors: int|None = None) -> list: # tipo de retorno es una lista
+#   result = db
+#   if size:
+#     result = [car for car in result if car.id == size];
+#   if doors:
+#     result = [car for car in result if car.doors >= doors]
+#   else: 
+#     return result
+  
+# usando sqlite
 @app.get("/api/cars")
-def get_cars(size: str|None = None, doors: int|None = None) -> list: # tipo de retorno es una lista
-  result = db
+def get_cars(size: str|None = None, doors: int|None = None,
+             session: Session = Depends(get_session)) -> list: # tipo de retorno es una lista
+  query = select(Car) # una query que selecciona datos de un objeto Car
   if size:
-    result = [car for car in result if car.id == size];
+    query = query.where(Car.size == size)
   if doors:
-    result = [car for car in result if car.doors >= doors]
-  else: 
-    return result
+    query = query.where(Car.doors == doors)
+  return session.exec(query).all()
+
 
 #al especificar el tipo de dato con 'type hints' python nos ayuda para saber que se debe enviar o retornar
-@app.get("/api/cars/{id}")
-def car_by_id(id: int) -> dict:
-  result = [car for car in db if car.id==id]
-  if result:
-    return result[0]
+@app.get("/api/cars/{id}", response_model=Car)
+def car_by_id(id: int,
+             session: Session = Depends(get_session)) -> Car:
+  car = session.get(Car, id)
+  if car:
+    return car
   else:
     raise HTTPException(status_code=404, detail=f"No car with id {id} in the db")
 
@@ -75,56 +92,54 @@ def car_by_id(id: int) -> dict:
 
 # funciona con sqlite
 @app.post("/api/cars", response_model=Car)
-def add_car(car_input: CarInput) -> Car:
+def add_car(car_input: CarInput,
+             session: Session = Depends(get_session)) -> Car:
   # trasaccion con la db, o todo esta bueno y se guarda o algo falla y no se guarda en la db
-  with Session(engine) as session:
-    new_car = Car.from_orm(car_input) # permite trabajara correctamente con relaciones entre objetos
-    session.add(new_car)
-    session.commit()
-    session.refresh(new_car) # en esta parte se crea el id cuando se salva en la db
-    return new_car
+  new_car = Car.from_orm(car_input) # permite trabajara correctamente con relaciones entre objetos
+  session.add(new_car)
+  session.commit()
+  session.refresh(new_car) # en esta parte se crea el id cuando se salva en la db
+  return new_car
   
 
 @app.delete("/api/cars/{id}", status_code=204)
-def remove_car(id: int) -> None:
-  matches = [car for car in db if car.id == id]
-  if matches:
-    car = matches[0];
-    db.remove(car);
-    save_db(db)
+def remove_car(id: int,
+             session: Session = Depends(get_session)) -> None:
+  car = session.get(Car, id)
+  if car:
+    session.delete()
+    session.commit()
   else:
     raise HTTPException(status_code=404, detail=f"No car with id = {id}")
   
 
-@app.put("/api/cars/{id}")
-def change_car(id: int, new_data: CarInput) -> CarOutput:
-  matches = [car for car in db if car.id == id]
-  if matches:
-    car = matches[0]
+@app.put("/api/cars/{id}", response_model=Car)
+def change_car(id: int, new_data: CarInput,
+             session: Session = Depends(get_session)) -> Car:
+  car = session.get(Car, id)
+  if car:
     car.fuel = new_data.fuel
     car.transmission = new_data.transmission
     car.size = new_data.size
     car.doors = new_data.doors
-    save_db(db)
+    session.commit()
     return car
   else:
     raise HTTPException(status_code=404, detail=f"No car with id = {id}")
 
 
-@app.post("/api/cars/{car_id}/trips", response_model=TripOutput)
-def add_trip(car_id: int, trip: TripInput) -> TripOutput:
-  matches = [car for car in db if car.id == id]
-  if matches:
-    car = matches[0]
-    new_trip = TripOutput(id=len(car.trips)+1, start=trip.start, 
-                          end=trip.end, description=trip.description)
+@app.post("/api/cars/{car_id}/trips", response_model=Trip)
+def add_trip(car_id: int, trip_input: TripInput,
+             session: Session = Depends(get_session)) -> Trip:
+  car = session.get(Car, car_id)
+  if car:
+    new_trip = Trip.from_orm(trip_input, update={'car_id': car_id})
     car.trips.append(new_trip)
-    save_db(db)
+    session.commit()
+    session.refresh(new_trip)
     return new_trip
   else:
     raise HTTPException(status_code=404, detail=f"No car with id = {id}")
-
-
 
 
 
--- schemas.py
+++ schemas.py
@@ -1,8 +1,32 @@
 # python -m pip install sqlmodel
 
-import json
+# import json
 # from pydantic import BaseModel # se reemplaza x SQLmodel que hereda de pydantic
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+
+
+class TripInput(SQLModel):
+  start: int
+  end: int
+  description: str
+  
+  
+class TripOutput(TripInput):
+  id: int
+
+
+class Trip(TripInput, table=True):
+  id: int | None = Field(default=None, primary_key=True)
+  # no es optional '|None' xq para q hayan viajes, debe existir 1 carro que los realice
+  car_id: int = Field(foreign_key="car.id")# id del carro que le pertene este 'Trip'
+  # 'Car' es un string, porque la clase 'Car' esta debajo de 'Trip' en este archivo,
+  #  lo que hace que cuando python lea el codido la clase 'Car' no existe aun,
+  # asi que no podemos usar una variable, por ello se usa un string y luego cuando
+  # la clase exista sera usada como el type hint
+  # back_populates, significa que la misma relacion existe en la clase 'Car',
+  # con un atributo llamado 'trips'
+  car: "Car" = Relationship(back_populates="trips") 
+
 
 class CarInput(SQLModel):
   size: str
@@ -19,37 +43,27 @@ class CarInput(SQLModel):
         "fuel" : "gasoline",
       }
     }
-
-# con table=true, se vuelve un modelo de datos que se envia a la db
-class Car(CarInput, table=True):
-  # solo cuando se salve el car en la db se crea el id
-  id: int | None =  Field(primary_key=True, default=None)
-  
-  
-
-class TripInput(SQLModel):
-  start: int
-  end: int
-  description: str
-  
-  
-class TripOutput(TripInput):
-  id: int
-  
+    
 # Hereda de CarInput pero le agrega el id y es muy similar a 'Car', solo que 
 # CarInput es un Schema que determina que se datos se enviaran por internet
 class CarOutput(CarInput):
   id: int
   trips: list[TripOutput] = []
+
+# con table=true, se vuelve un modelo de datos que se envia a la db
+class Car(CarInput, table=True):
+  # solo cuando se salve el car en la db se crea el id
+  id: int | None =  Field(primary_key=True, default=None)
+  trips: list[Trip] = Relationship(back_populates="car") # muchos viajes a un carro
+
   
+# def load_db() -> list[CarOutput]:
+#   """Load a list of CarInput Objects from a JSON file"""
+#   with open("cars.json") as f:
+#     # haga esto 'Car.parse...' para cada 'obj' en el archivo 'f' transformado a json...
+#     return [CarOutput.parse_obj(obj) for obj in json.load(f)]
   
-def load_db() -> list[CarOutput]:
-  """Load a list of CarInput Objects from a JSON file"""
-  with open("cars.json") as f:
-    # haga esto 'Car.parse...' para cada 'obj' en el archivo 'f' transformado a json...
-    return [CarOutput.parse_obj(obj) for obj in json.load(f)]
-  
-def save_db(cars: list[CarInput]):
-  with open("cars.json", 'w') as f:
-    #cada carro de la lista cars, lo convierte en un dict con espaciado 4
-    json.dump([car.dict() for car in cars], f, indent=4)
+# def save_db(cars: list[CarInput]):
+#   with open("cars.json", 'w') as f:
+#     #cada carro de la lista cars, lo convierte en un dict con espaciado 4
+#     json.dump([car.dict() for car in cars], f, indent=4)
